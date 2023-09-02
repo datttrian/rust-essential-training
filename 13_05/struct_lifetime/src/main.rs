1a885cf28106a2c2dbf6726ad3fa60c5ed3bb0f6
datttrian <datttrian@gmail.com>
2023-09-02 09:14:13 +0000

    Struct lifetime annotations

---
+++ 13_05/struct_lifetime/src/main.rs
@@ -0,0 +1,18 @@
+struct Shuttle<'a> {
+    name: &'a str,
+}
+
+impl<'a, 'b> Shuttle<'a> {
+    fn send_transmission(&'a self, msg: &'b str) -> &'b str {
+        println!("Transmitting message: {}", msg);
+        // self.name
+        msg
+    }
+}
+
+fn main() {
+    let vehicle = Shuttle { name: "Endeavour" };
+
+    let sender = vehicle.send_transmission("Greetings from orbit!");
+    println!("sender is {}", sender);
+}
